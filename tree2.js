16d9a5ae5fc47055053d5f53b5b1ad24363e5001
hojooneum <bldolphin96@gmail.com>
2019-12-28 01:40:16 +0900

    svgjs prototype added

--- tree2.js
+++ tree2.js
@@ -9,8 +9,8 @@ class Node {
     this.y = y;
     this.id = id;
     this.label = label;
-    this.size = 50;
-    this.color = "#8f8";
+    this.size = 80;
+    this.color = "#f38";
   }
 }
 
@@ -20,19 +20,33 @@ class Line {
     this.target = target;
     this.id = id;
     this.size = 3;
-    this.color = "#cf8";
+    this.color = "#000";
     this.label = "line";
   }
 }
 
 // 노드 생성
-const n1 = new Node(100, 30, "n1", "10");
-const n2 = new Node(70, 60, "n2", "5");
-const n3 = new Node(130, 60, "n3", "5");
+const n1 = new Node(70, 100, "n1", "10");
+const n2 = new Node(69, 101, "n2", "5");
+const n3 = new Node(71, 101, "n3", "5");
 console.log(n1);
 g.nodes.push(n1);
 g.nodes.push(n2);
 g.nodes.push(n3);
+//실험 노드 하나 생성
+// const aNode = {
+//   id: "n4",
+//   label: "3",
+//   x: 68,
+//   y: 102,
+//   size: 70,
+//   color: "#8f8",
+//   leftChild: true,
+//   rightChild: false
+// };
+// g.nodes.push(aNode);
+
+//실험 라인 생성
 
 const e1 = new Line("n1", "n2", "e1");
 const e2 = new Line("n1", "n3", "e2");
@@ -64,5 +78,29 @@ const s = new sigma({
   renderer: {
     container: document.getElementById("canvas"),
     type: "canvas"
+  },
+  settings: {
+    scalingMode: "inside",
+    sideMargin: 10,
+    minNodeSize: 1,
+    maxNodeSize: 3,
+    minEdgeSize: 0.5,
+    maxEdgeSize: 2
+    zoomMin: 0.5
   }
 });
+
+// 예시 자료형
+
+const json = {
+    root: {
+        val: 10,
+        leftChild: {
+            val: 5
+        }
+        rightChild: {
+            val: 15,
+            rightChild: 16
+        }
+    }
+}
